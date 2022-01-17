bdb6e622c58729f94ed973a729c57d8a9e894091
Hugo Huart <hugo.huart@heig-vd.ch>
2022-01-17 21:21:49 +0100

    Update starting scripts

--- step3/scripts/start-dynamic.sh
+++ step3/scripts/start-dynamic.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-docker run -d --rm --hostname --name dynamic_http huart1848/dynamic-http
+docker run -d --rm --hostname dynamic-http --name dynamic_http huart1848/dynamic-http
--- step3/scripts/start-static.sh
+++ step3/scripts/start-static.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-docker run -d --rm --name static_http huart1848/static-http
+docker run -d --rm --hostname static-http --name static_http huart1848/static-http
